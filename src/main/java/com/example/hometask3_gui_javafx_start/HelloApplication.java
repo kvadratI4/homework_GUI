829aba7c314f477b45dd8c24b987df9f903cebfb
kvadratI4 <kvardonovdima@gmail.com>
2022-04-24 21:17:35 +0300

    Area calculating method was added

--- src/main/java/com/example/hometask3_gui_javafx_start/HelloApplication.java
+++ src/main/java/com/example/hometask3_gui_javafx_start/HelloApplication.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, InterruptedException {
         Group group = new Group();
         Scene scene = new Scene(group, 800, 800);
 
@@ -56,6 +56,8 @@ public class HelloApplication extends Application {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 scene.setCursor(Cursor.DEFAULT);
+                isCornerInOtherArea(getFirstCoords_(first_button), getSecondCoords_(second_button));
+                System.out.println("Total area: " + areaCalculating(getFirstCoords_(first_button), getSecondCoords_(second_button)));
             }
         });
 
@@ -64,10 +66,10 @@ public class HelloApplication extends Application {
             public void handle(MouseEvent mouseEvent) {
                 first_button.setLayoutX(mouseEvent.getSceneX() - firstButtonCoords[0]);
                 first_button.setLayoutY(mouseEvent.getSceneY() - firstButtonCoords[1]);
-                System.out.println(Arrays.toString(firstButtonCoords));
             }
         });
         /* -------->  */
+
 
         /* SECOND_BUTTON */
         second_button.setOnMousePressed(new EventHandler<MouseEvent>() {
@@ -85,6 +87,8 @@ public class HelloApplication extends Application {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 scene.setCursor(Cursor.DEFAULT);
+                isCornerInOtherArea(getFirstCoords_(first_button), getSecondCoords_(second_button));
+                System.out.println("Total area: " + areaCalculating(getFirstCoords_(first_button), getSecondCoords_(second_button)));
             }
         });
 
@@ -93,9 +97,57 @@ public class HelloApplication extends Application {
             public void handle(MouseEvent mouseEvent) {
                 second_button.setLayoutX(mouseEvent.getSceneX() - secondButtonCoords[0]);
                 second_button.setLayoutY(mouseEvent.getSceneY() - secondButtonCoords[1]);
-                System.out.println(Arrays.toString(secondButtonCoords));
             }
         });
+        /* -------->  */
+    }
+
+
+    /* BUTTONS COORDINATES */
+    public void isCornerInOtherArea(double [][] firstButtonCoords_, double [][] secondButtonCoords_){
+        System.out.println("first button: " + Arrays.deepToString(firstButtonCoords_));
+        System.out.println("second button: " + Arrays.deepToString(secondButtonCoords_));
+        System.out.println("=================================");
+
+    }
+
+    public double[][] getFirstCoords_(Button first_button) {
+        final double[][] firstButtonCoords_ = {
+                {first_button.getLayoutX(), first_button.getLayoutY()},
+                {first_button.getLayoutX() + 100, first_button.getLayoutY()},
+                {first_button.getLayoutX() + 100, first_button.getLayoutY() + 100},
+                {first_button.getLayoutX(), first_button.getLayoutY() + 100}
+        };
+
+        return firstButtonCoords_;
+    }
+
+    public double[][] getSecondCoords_(Button second_button) {
+        final double[][] secondButtonCoords_ = {
+                {second_button.getLayoutX(), second_button.getLayoutY()},
+                {second_button.getLayoutX() + 100, second_button.getLayoutY()},
+                {second_button.getLayoutX() + 100, second_button.getLayoutY() + 100},
+                {second_button.getLayoutX(), second_button.getLayoutY() + 100}
+        };
+
+        return secondButtonCoords_;
+        /* -------->  */
+    }
+
+
+    /* AREA CALCULATING */
+    public double areaCalculating(double [][] firstButtonCoords_, double [][] secondButtonCoords_) {
+        double left = Math.max(firstButtonCoords_[0][0], secondButtonCoords_[0][0]);
+        double bottom = Math.max(firstButtonCoords_[0][1], secondButtonCoords_[0][1]);
+        double top = Math.min(firstButtonCoords_[2][1], secondButtonCoords_[2][1]);
+        double right = Math.min(firstButtonCoords_[2][0], secondButtonCoords_[2][0]);
+
+        double width = right - left;
+        double height = top - bottom;
+
+        if (width < 0 || height < 0) {
+            return 0; }
+        return width * height;
         /* -------->  */
     }
 
